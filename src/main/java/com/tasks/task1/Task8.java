55c2cccb98503b503cf5b2c6f400124f9fb0959b
AlexeyNS <webxak31@yandex.ru>
2022-08-03 11:35:38 +0300

    сумма чисел

---
+++ src/main/java/com/tasks/task1/Task8.java
@@ -0,0 +1,19 @@
+package com.tasks.task1;
+
+import java.util.Scanner;
+
+/**
+ * Задача 7: Программа выводит сумму чисел, введенных пользователем.
+ */
+public class Task8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите число:");
+        int a = sc.nextInt();
+        System.out.print("Введите число:");
+        int b = sc.nextInt();
+        sc.close();
+        System.out.println("Сумма: " + (a + b));
+    }
+}
