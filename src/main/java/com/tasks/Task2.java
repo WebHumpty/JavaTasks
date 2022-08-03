8226086a5631b60edb7dde4b107be7810db05f9a
AlexeyNS <webxak31@yandex.ru>
2022-08-03 11:11:22 +0300

    Случайное число

---
+++ src/main/java/com/tasks/Task2.java
@@ -0,0 +1,13 @@
+package com.tasks;
+
+/**
+ * Задача 1: Программа выдает случайное число от 0 до 9
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+        int n = (int)(Math.random() * 10);
+
+        System.out.println("Случайное число: " + n);
+    }
+}
