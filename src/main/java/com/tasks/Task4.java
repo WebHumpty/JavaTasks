ef6f03fdde68deba195116f7f84c09bb22db6271
AlexeyNS <webxak31@yandex.ru>
2022-08-03 11:16:44 +0300

    случайное число от 1 до 10

---
+++ src/main/java/com/tasks/Task4.java
@@ -0,0 +1,13 @@
+package com.tasks;
+
+/**
+ * Задача 3: Программа выдает случайное число от 1 до 10.
+ */
+public class Task4 {
+
+    public static void main(String[] args) {
+        int n = (int)(Math.random() * 10 + 1);
+
+        System.out.println("Случайное число: " + n);
+    }
+}
