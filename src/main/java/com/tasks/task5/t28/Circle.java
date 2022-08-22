ae75c03197ea8e629b9605a242f9a55d1408bd5e
WebHumpty <webxak31@yandex.ru>
2022-08-22 10:19:38 +0300

    вывести площадь сектора и длину круга от угла a до угла b

---
+++ src/main/java/com/tasks/task5/t28/Circle.java
@@ -0,0 +1,19 @@
+package com.tasks.task5.t28;
+
+public class Circle {
+
+    String color;
+    int radius;
+
+    public double areaS(int a, int b) {
+        int angle = Math.abs(a - b);
+        double area = Math.PI * radius * radius * angle / 360;
+        return area;
+    }
+
+    public double lengthOfArk(int a, int b) {
+        int angle = Math.abs(a - b);
+        double area = 2 * Math.PI * radius * angle / 360;
+        return area;
+    }
+}
