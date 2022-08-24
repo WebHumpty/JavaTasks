27e63adc20c528c3a20a7e1d41ac2bf5cae5ce0c
WebHumpty <webxak31@yandex.ru>
2022-08-24 09:12:02 +0300

    вывести сообщение о расстоянии от солнца до земли

---
+++ src/main/java/com/tasks/task5/t32/Speed.java
@@ -0,0 +1,8 @@
+package com.tasks.task5.t32;
+
+public class Speed {
+
+    static long speedOfLights = 300000;
+    static double aOfGravity = 9.81;
+    static long distanceFromEarthToSun = 150000000;
+}
