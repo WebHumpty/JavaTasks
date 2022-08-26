0999abb27a64b1d939b68b7461e471412f9fdcee
WebHumpty <webxak31@yandex.ru>
2022-08-26 09:40:21 +0300

    вывести площадь круга, сектора, окружности и дуги круга

---
+++ src/main/java/com/tasks/task5/t36/Main.java
@@ -0,0 +1,24 @@
+package com.tasks.task5.t36;
+
+/**
+ * Задача 126:
+ * Программа выводит площадь круга, площадь сектора,
+ * длину окружности и длину дуги.
+ * Радиус и углы формируются случайным образом.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int angleA = (int)(Math.random() * 360) + 1;
+        int angleB = (int)(Math.random() * 360) + 1;
+        int radius = (int)(Math.random() * 10) + 1;
+
+        System.out.println(Circle.nameOfCircle);
+        System.out.println("Радиус " + radius);
+        System.out.println("Между углами " + angleA + " и " + angleB);
+        System.out.println("Площадь круга " + Circle.areaOfC(radius));
+        System.out.println("Площадь сектора " + Circle.areaOfS(angleA, angleB, radius));
+        System.out.println("Длина окружности " + Circle.lengthOfCircle(radius));
+        System.out.println("Длина дуги " + Circle.lengthOfArk(angleA, angleB, radius));
+    }
+}
