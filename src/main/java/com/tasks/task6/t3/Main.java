6c43295195e8acd14796ce8c79e73a3bdca524eb
WebHumpty <webxak31@yandex.ru>
2022-08-28 12:02:16 +0300

    вывести квадрат числа 5

---
+++ src/main/java/com/tasks/task6/t3/Main.java
@@ -0,0 +1,23 @@
+package com.tasks.task6.t3;
+
+/**
+ * Задача 132:
+ * Программа выводит квадрат числа 5
+ * вне зависимости от типов входящих данных.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int ai = 5;
+        String as = "5";
+        long al = 5;
+        short ash = 5;
+        char ac = '5';
+
+        Square.degree(ai);
+        Square.degree(as);
+        Square.degree(al);
+        Square.degree(ash);
+        Square.degree(ac);
+    }
+}
---
+++ src/main/java/com/tasks/task6/t3/Square.java
@@ -0,0 +1,28 @@
+package com.tasks.task6.t3;
+
+public class Square {
+
+    public static void degree(int a) {
+        System.out.println("Квадрат числа 5 = " + (a * a));
+    }
+
+    public static void degree(String a) {
+        int b = Integer.parseInt(a);
+        System.out.println("Квадрат числа 5 = " + (b * b));
+    }
+
+    public static void degree(long a) {
+        int b = (int)a;
+        System.out.println("Квадрат числа 5 = " + (b * b));
+    }
+
+    public static void degree(short a) {
+        int b = (int)a;
+        System.out.println("Квадрат числа 5 = " + (b * b));
+    }
+
+    public static void degree(char a) {
+        int b = a - 48;
+        System.out.println("Квадрат числа 5 = " + (b * b));
+    }
+}
