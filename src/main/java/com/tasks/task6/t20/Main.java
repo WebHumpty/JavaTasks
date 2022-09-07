6f9559a157857f54b12f176add6a3c8b25f5e8ce
WebHumpty <webxak31@yandex.ru>
2022-09-07 10:51:00 +0300

    вывести характеристики авто

---
+++ src/main/java/com/tasks/task6/t20/Main.java
@@ -0,0 +1,20 @@
+package com.tasks.task6.t20;
+
+/**
+ * Задача 149:
+ * Программа выводит характеристики трех одинаковых автомобилей, поступивших в салон.
+ * Если характеристики не заданы, int переменным присваивается 0,
+ * String переменным присваивается null.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        car1.outCar();
+        car2.outCar();
+        car3.outCar();
+    }
+}
