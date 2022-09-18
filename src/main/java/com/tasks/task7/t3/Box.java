7d4658ca0ed9de92bd1f2c3d8bb65b8b1afb00a4
WebHumpty <webxak31@yandex.ru>
2022-09-18 10:20:05 +0300

    увеличить размер коробки

---
+++ src/main/java/com/tasks/task7/t3/Box.java
@@ -0,0 +1,32 @@
+package com.tasks.task7.t3;
+
+public class Box {
+
+    public int width;
+    public int height;
+    public int length;
+
+    public Box(int width, int height, int length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    public void increase1(int width, int height, int length) {
+        System.out.println();
+        width = width * 2;
+        height = height * 2;
+        length = length * 2;
+        System.out.println("по значению");
+        System.out.println();
+    }
+
+    public void increase2(Box box) {
+        System.out.println();
+        box.width = box.width * 2;
+        box.height = box.height * 2;
+        box.length = box.length * 2;
+        System.out.println("по ссылке");
+        System.out.println();
+    }
+}
